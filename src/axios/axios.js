8d3cd47aef50f101f3edd09cd71a7df0e0cfc9e5
zhangshuo <443494891@qq.com>
2020-04-02 08:46:54 +0800

    打包后跨域

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -6,6 +6,7 @@ import qs from 'qs';
 Vue.use(VueAxios, axios);
 
 axios.defaults.withCredentials = true;
+axios.defaults.timeout = 1000 * 10; 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 axios.defaults.baseURL = 'http://www.zhuoran.fun:3000/';
 
